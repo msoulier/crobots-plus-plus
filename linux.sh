91ec975804d6a03e3be635d79759765871c36cde
Michael P. Soulier <msoulier@digitaltorque.ca>
2025-08-04 14:34:56 -0400

    Added some helpful targets to linux.sh

--- linux.sh
+++ linux.sh
@@ -12,6 +12,8 @@ if [ "x$action" = "x" ]; then
     echo "  release: build release binary" 1>&2
     echo "  run: run build locally" 1>&2
     echo "  clean: clean up build output" 1>&2
+    echo "  tags: run ctags on all source" 1>&2
+    echo "  dos2unix: run dos2unix on all source files" 1>&2
     exit 1
 fi
 
@@ -31,7 +33,7 @@ run()
 
 clean()
 {
-    rm -rf build
+    rm -rf build tags *.log
 }
 
 case "$action" in
@@ -50,6 +52,15 @@ case "$action" in
 
     clean)
         clean
+        ;;
+
+    dos2unix)
+        find include src \( -name "*.cpp" -o -name "*.hpp" -o -name "*.md" \) -print0 | xargs -0 dos2unix
+        ;;
+
+    tags)
+        rm -f tags
+        find . \( -name "*.cpp" -o -name "*.hpp" \) -print0 | xargs -0 ctags -a
         ;;
 
     *)
