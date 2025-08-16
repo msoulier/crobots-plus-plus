6a74f421cf5d64265ac1f6773887e86a8b4a9617
Jaan Soulier <jaansoulier@gmail.com>
2025-08-16 15:05:46 -0400

    extern C ordering

--- include/Crobots++/Crobots++.hpp
+++ include/Crobots++/Crobots++.hpp
@@ -3,7 +3,7 @@
 #if defined(_WIN32) || defined(__CYGWIN__)
 #define CROBOTS_ENTRYPOINT __declspec(dllexport)
 #elif defined(__GNUC__) || defined(__clang__)
-#define CROBOTS_ENTRYPOINT __attribute__((visibility("default"))) extern "C"
+#define CROBOTS_ENTRYPOINT extern "C" __attribute__((visibility("default")))
 #else
 #define CROBOTS_ENTRYPOINT extern "C"
 #endif
